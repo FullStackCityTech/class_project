b956a71d50832cbd90d9e03d3e670ff2b0f14913
FullStackCityTech <kaiba.corporation.llc@gmail.com>
2022-08-30 19:29:02 -0400

    ajax

---
+++ 0830_ajax/info.php
@@ -0,0 +1,17 @@
+<?php
+echo "programming languages: ";
+
+echo __FILE__.' '; 
+//exit;
+
+
+?>
+ 
+
+<div id="container">
+
+<?php  echo 'line #: '. __LINE__.' '; ?>
+gfonfijnbifjnifnbkfknbkofkmbkf
+foknfdonofkbmfkokbvfkdokvnokf 
+gijfjijfnjfjnjfjjfjnjvfjmkk mk kk m
+</div>
